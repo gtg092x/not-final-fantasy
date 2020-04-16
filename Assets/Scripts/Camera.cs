15166b2aafb139f8882819c07645a870763411de
Matthew Drake <mdrake@riotgames.com>
2020-04-15 20:56:47 -0700

    saved animation

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using System.Linq;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject[] focusTargets = new GameObject[] { };
+    public void ChangeFocus() {
+        var activeVc = GetComponent<CinemachineBrain>().ActiveVirtualCamera;
+        var currentFocus = activeVc.Follow;
+        var currentFocusIndex = focusTargets.ToList().IndexOf(currentFocus.gameObject);
+        var nextFocusIndex = currentFocusIndex >= focusTargets.Length - 1
+            ? 0
+            : currentFocusIndex + 1;
+        activeVc.Follow = focusTargets[nextFocusIndex].transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    public float Speed = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,32 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateMovement();
+    }
+
+    void UpdateMovement() {
+        float speed = this.Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            speed = speed * 3;
+        }
+        var Offset = new Vector2(0f, 0f);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Offset = Offset + Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Offset = Offset + Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Offset = Offset + Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Offset = Offset + Vector2.down;
+        }
+        GetComponent<Rigidbody2D>().AddRelativeForce(Offset * speed);
+       
     }
 }
